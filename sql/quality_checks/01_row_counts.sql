c21d01cf598151debfcb824fe5fd9aa46eb19f93
apsket <albertopineda@schlamme.com>
2026-05-03 12:19:54 -0600

    Enhance row counts and sanity checks SQLs

--- sql/quality_checks/01_row_counts.sql
+++ sql/quality_checks/01_row_counts.sql
@@ -1,11 +1,15 @@
-SELECT 'orders' AS table_name, COUNT(*) FROM raw.orders
+SELECT 'orders' AS table_name, COUNT(*) AS row_count, COUNT(DISTINCT order_id) AS unique_ids FROM raw.orders
 UNION ALL
-SELECT 'order_items', COUNT(*) FROM raw.order_items
+SELECT 'order_items', COUNT(*), COUNT(DISTINCT order_id) FROM raw.order_items
 UNION ALL
-SELECT 'customers', COUNT(*) FROM raw.customers
+SELECT 'customers', COUNT(*), COUNT(DISTINCT customer_unique_id) FROM raw.customers
 UNION ALL
-SELECT 'products', COUNT(*) FROM raw.products
+SELECT 'products', COUNT(*), COUNT(DISTINCT product_id) FROM raw.products
 UNION ALL
-SELECT 'reviews', COUNT(*) FROM raw.reviews
+SELECT 'reviews', COUNT(*), COUNT(DISTINCT review_id) FROM raw.reviews
 UNION ALL
-SELECT 'payments', COUNT(*) FROM raw.payments;
+SELECT 'payments', COUNT(*), COUNT(DISTINCT order_id) FROM raw.payments
+UNION ALL
+SELECT 'sellers', COUNT(*), COUNT(DISTINCT seller_id) FROM raw.sellers
+UNION ALL
+select 'geolocation', COUNT(*), COUNT(DISTINCT geolocation_zip_code_prefix) FROM raw.geolocation;
--- sql/quality_checks/04_sanity_checks.sql
+++ sql/quality_checks/04_sanity_checks.sql
@@ -1,10 +1,10 @@
-SELECT COUNT(*)
+SELECT COUNT(*) as num_invalid_order_dates
 FROM raw.orders
 WHERE order_delivered_customer_date < order_purchase_timestamp;
 
-SELECT MIN(payment_value), MAX(payment_value)
+SELECT MIN(payment_value) as min_payment_value, MAX(payment_value) as max_payment_value
 FROM raw.payments;
 
-SELECT *
+SELECT COUNT(*) as num_invalid_review_scores
 FROM raw.reviews
 WHERE review_score < 1 OR review_score > 5;
